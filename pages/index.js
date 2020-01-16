235247e4584597ff969f21e79a522ca3aa37e711
its-tayo <akindolietayo@gmail.com>
2020-01-16 09:56:53 +0100

    style(tailwind.css): add typography

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
 const Home = () => (
   <div className="p-4 shadow rounded bg-white">
-    <h1 className="text-purple-500 leading-normal">Tailwind!!!</h1>
+    <h1>Scizzor</h1>
+    <h2>Scizzor</h2>
+    <h3>Scizzor</h3>
+    <h4>Scizzor</h4>
+    <p className="p-big">dsdsd</p>
+    <p>dsdsd</p>
+    <p className="p-small">dsdsd</p>
   </div>
 );
 
