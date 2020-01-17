0eec052f5fd0b6e0a4321002801c29486acc9455
its-tayo <akindolietayo@gmail.com>
2020-01-17 12:06:56 +0100

    style(tailwind.config.js): add colors

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 const Home = () => (
   <div className="p-4 shadow rounded bg-white">
-    <h1>Scizzor</h1>
-    <h2>Scizzor</h2>
-    <h3>Scizzor</h3>
-    <h4>Scizzor</h4>
-    <p className="p-big">dsdsd</p>
-    <p>dsdsd</p>
-    <p className="p-small">dsdsd</p>
+    <h1 className="text-primary">Scizzor</h1>
+    <h2 className="text-secondary">Scizzor</h2>
+    <h3 className="text-tertiary">Scizzor</h3>
+    <h4 className="text-alt-1">Scizzor</h4>
+    <p className="p-big text-alt-2">Scizzor</p>
+    <p>Scizzor</p>
+    <p className="p-small">Scizzor</p>
   </div>
 );
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,18 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: "#C09558",
+        secondary: "#696E95",
+        tertiary: "#F8F3EE",
+
+        alt: {
+          1: "#262627",
+          2: "#DFE2E6",
+          3: "#FFFFFF"
+        }
+      }
+    }
   },
   variants: {},
   plugins: []
